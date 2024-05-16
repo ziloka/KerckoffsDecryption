3747addc23b956fefe1f5474e43d8ebdac184823
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-05-16 13:47:34 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,26 +1,23 @@
 import re
 from collections import deque
 
+string = open("input.txt", "r").read()
 contents = open("part1.txt", "r").read().splitlines()
 # https://stackoverflow.com/a/12739974
-codemap = dict(zip(*[iter(contents)]*2))
-
-# swap the keys and the values
-codemap = {v: k for k, v in codemap.items()}
-string = open("input.txt", "r").read()
+codemap = {v: k for k, v in dict(zip(*[iter(contents)]*2)).items()}
 
 # https://stackoverflow.com/a/21139641
-def dict_roll(dct, n):
-    shift_values = deque(dct.values())
-    shift_values.rotate(n)
-    return dict(zip(dct.keys(), shift_values))
+def dict_shift_keys(dct, n):
+    shift_keys = deque(dct.keys())
+    shift_keys.rotate(n)
+    return dict(zip(shift_keys, dct.values()))
 
 while any(char.isdigit() for char in string):
     result = re.search("\d+", string)
-    length = string[result.start()]
-    code = string[result.start():result.start() + int(length)]
-    # print(f": {result.start()} {result.end()} {length} {code}\n")
+    start = result.start()
+    code = string[start:start + int(string[start])]
     string = string.replace(code, codemap[code], 1)
-    dict_roll(codemap, int(code[-1]))
+    # print(f"{codemap}\n{code[-1]}\n")
+    codemap = dict_shift_keys(codemap, int(code[-1]))
 
 print(string)
